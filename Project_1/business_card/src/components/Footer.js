e5f1cc454d22e255f0a57498b67e1fbc80053a7f
Peter Crowther <p.crowther@qub.ac.uk>
2022-07-02 06:36:33 +0100

    UPDATE - Social media icons plus relative styling.

--- Project_1/business_card/src/components/Footer.js
+++ Project_1/business_card/src/components/Footer.js
@@ -1,4 +1,8 @@
 import React from "react";
+import facebook from "../images/facebook.png";
+import twitter from "../images/twitter.png";
+import instagram from "../images/instagram.png";
+import pinterest from "../images/pinterest.png";
 
 export default Footer;
 
@@ -6,10 +10,10 @@ function Footer() {
     return (
         <footer>
             <ul className="social-media">
-                <li className="icon twitter"></li>
-                <li className="icon facebook"></li>
-                <li className="icon snapchat"></li>
-                <li className="icon twitter"></li>                                                
+                <li className="icon twitter"><img src={twitter} alt="twitter" /></li>
+                <li className="icon facebook"><img src={facebook} alt="facebook" /></li>
+                <li className="icon instagram"><img src={instagram} alt="instagram" /></li>
+                <li className="icon pinterest"><img src={pinterest} alt="pinterest" /></li>                                                
             </ul>
         </footer>
     );
--- Project_1/business_card/src/components/Info.js
+++ Project_1/business_card/src/components/Info.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Big_Cow from "../images/big_cow_800W.jpg"
-
-
+import Big_Cow from "../images/big_cow_800W_2.jpg";
+import email from "../images/email.png";
+import linkedin from "../images/linkedin.png";
 
 export default Info;
 
@@ -14,8 +14,8 @@ function Info() {
                 <h2>Archie's Favourite</h2>
                 <p></p>
                 <div className="button--cont">
-                    <button className="email"> Email</button>
-                    <button className="linkedin">LinkedIn</button>
+                    <button className="email social-media"><img src={email} alt="email" /> Email</button>
+                    <button className="linkedin social-media"><img src={linkedin} alt="linkedin" /> LinkedIn</button>
                 </div>
             </div>
         </div>
