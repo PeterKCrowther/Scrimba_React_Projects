cbdf40d97a3ae88da83e6e4cd7d7328d52ef7378
Peter K Crowther <petercrowther@hotmail.com>
2022-08-23 16:28:48 +0100

    UPDATE - Project 3 basic folder/file creation

---
+++ Project_3/meme-generator/src/components/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Form() {
+    return (
+        <section className="form-cont">
+            <form>
+                <input className="first-line" />
+                <input className="second-line" />
+                <input className="submit--button" type="submit" >Get a new meme image</input>
+            </form>
+        </section>
+    );
+
+}
---
+++ Project_3/meme-generator/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header () {
+    return (
+        <section className="header">
+            <img className="header--logo" src="./images/Logo.png" alt="Meme Generator Logo" />
+            <h3>React Course - Project 3</h3>
+        </section>
+    );
+}
