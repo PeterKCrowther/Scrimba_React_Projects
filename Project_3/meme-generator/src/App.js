1919d33a450da77a1b5c858376deeb0481e164c5
Peter K Crowther <petercrowther@hotmail.com>
2022-08-23 17:06:05 +0100

    UPDATE - Form html

--- Project_3/meme-generator/src/App.js
+++ Project_3/meme-generator/src/App.js
@@ -2,11 +2,13 @@ import './css/style.css';
 
 
 import Header from './components/Header.js';
+import FormDetails from './components/FormDetails.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <FormDetails />
     </div>
   );
 }
--- Project_3/meme-generator/src/components/FormDetails.js
+++ Project_3/meme-generator/src/components/FormDetails.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function Form() {
-    return (
+export default function FormDetails() {
+    return ( 
         <section className="form-cont">
             <form>
                 <input className="first-line" />
                 <input className="second-line" />
-                <input className="submit--button" type="submit" >Get a new meme image</input>
+                {/* <input className="submit--button" type="submit" >Get a new meme image</input> */}
             </form>
         </section>
     );
