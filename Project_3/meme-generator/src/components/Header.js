661fe53645160a0cc40fd0a223ac2b756d338ed0
Peter K Crowther <petercrowther@hotmail.com>
2022-08-26 15:09:31 +0100

    UPDATE - Header information and styling

--- Project_3/meme-generator/src/components/Header.js
+++ Project_3/meme-generator/src/components/Header.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Header () {
     return (
-        <section className="header">
+        <header className="header">
             <img className="header--logo" src="./images/Logo.png" alt="Meme Generator Logo" />
             <h3>React Course - Project 3</h3>
-        </section>
+        </header>
     );
 }
