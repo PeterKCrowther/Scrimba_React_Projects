c131a846348b4d93226b6138141ae56b6ae29c25
Peter K Crowther <petercrowther@hotmail.com>
2022-11-29 15:29:47 +0000

    UPDATE - PR4 - Create single die instances and styling update.

--- Project_4/tenzies_game/src/App.js
+++ Project_4/tenzies_game/src/App.js
@@ -3,10 +3,24 @@ import './App.css';
 import Die from './components/Die.js';
 
 function App() {
+
+  let randomNumber1to6 = () => Math.floor(Math.random() * 6 + 1);
+
   return (
     <div className="App">
       <main>
-        <Die />
+        <div className="die-cont">
+          <Die value={randomNumber1to6()} />
+          <Die value={randomNumber1to6()} />
+          <Die value={randomNumber1to6()} />
+          <Die value={randomNumber1to6()} />
+          <Die value={randomNumber1to6()} />
+          <Die value={randomNumber1to6()} />
+          <Die value={randomNumber1to6()} />
+          <Die value={randomNumber1to6()} />
+          <Die value={randomNumber1to6()} />
+          <Die value={randomNumber1to6()} />
+        </div>  
       </main>
     </div>
   );
