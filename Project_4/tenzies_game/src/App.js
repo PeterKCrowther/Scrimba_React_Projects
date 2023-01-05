80b8badd898f7f2dbf531fdc9a7e9af75a402771
Peter K Crowther <petercrowther@hotmail.com>
2023-01-05 12:14:27 +0000

    PR4 - UPDATE - conditional rendering for <Confetti />

--- Project_4/tenzies_game/src/App.js
+++ Project_4/tenzies_game/src/App.js
@@ -97,7 +97,7 @@ function App() {
   return (
     <div className="App">
       <main>
-        { (tenzies) ? <Confetti /> : "" }
+        { tenzies && <Confetti /> }
         <h1 className="title">Tenzies</h1>
         <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className="die-cont">
