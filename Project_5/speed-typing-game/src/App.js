5dd61e89a1b287f10da1066b2ec2e12af3ccdf62
Peter K Crowther <petercrowther@hotmail.com>
2023-02-06 10:26:46 +0000

    PR5 - SETUP - Basic App JSX

--- Project_5/speed-typing-game/src/App.js
+++ Project_5/speed-typing-game/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/styles.css';
+
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <h1>How fast can you type?</h1>
+            <textarea />
+            <h4>Time Remaining</h4>
+            <button>My Button</button>
+            <h1>Word Count</h1>
+        </>
+    )
 }
 
 export default App;
