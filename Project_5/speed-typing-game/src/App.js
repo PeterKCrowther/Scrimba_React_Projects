e34ab70729d62da0a12f7e2a4f2a3e64afa4777d
Peter K Crowther <petercrowther@hotmail.com>
2023-02-06 11:57:22 +0000

    PR5 - UPDATE - new fn countWords

--- Project_5/speed-typing-game/src/App.js
+++ Project_5/speed-typing-game/src/App.js
@@ -4,25 +4,34 @@ import React, {useState} from "react";
 
 function App() {
     
-  const [textArea, setTextArea] = useState({inputText: ""});
+  const [text, setText] = useState("");
   
   function textChange(event) {
-      const {name, value} = event.target;   
-      setTextArea(prevStat => {
-          return {...prevStat, [name]: value }
-      })
+      const {value} = event.target;   
+      setText(value);
+      // setText(prevStat => {
+      //     return {...prevStat, [name]: value }
+      // })
+  }
+
+  function countWords(text) {
+    const spacesFoundArr = text.match(/[^\s]+/g);
+    console.log( spacesFoundArr ? spacesFoundArr.length : 0);
   }
   
   return (
       <div>
           <h1>How fast do you type?</h1>
           <textarea 
-              value={textArea.inputText}
+              value={text.inputText}
               name="inputText"
               onChange={textChange}
           />
           <h4>Time reminaing: ???</h4>
-          <button>Start</button>
+          <button
+                onClick={() => countWords(text)}>
+                Start
+            </button>
           <h1>Word count: ???</h1>
       </div>
   )
