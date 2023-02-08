fc85c0c612e98a44b09ab95bd96fee9e5aeb1684
Peter K Crowther <petercrowther@hotmail.com>
2023-02-08 15:12:38 +0000

    PR5 - UPDATE - set gameStarted var to false at game end

--- Project_5/speed-typing-game/src/App.js
+++ Project_5/speed-typing-game/src/App.js
@@ -5,18 +5,21 @@ import React, {useState, useEffect} from "react";
 function App() {
     
   const [text, setText] = useState("");
-  const [timeLeft, setTimeLeft] = useState(10);
-  const [startGame, setStartGame] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(5);
+  const [gameStarted, setGameStarted] = useState(false);
  
   useEffect(() => {     
     console.log("Time left: " + timeLeft)
 
-    if ( startGame && timeLeft > 0 ) {
+    if ( gameStarted && timeLeft > 0 ) {
       setTimeout(() => {
         setTimeLeft( (time) => ( time-1 ) )
       }, (1000)); 
-    }   
-}, [startGame, timeLeft]);    
+    } else if ( timeLeft === 0 ) { //Timer at zero
+      setGameStarted(false)
+      console.log("isTimeRunning set to false");      
+    }
+}, [gameStarted, timeLeft]);    
 
   
   function textChange(event) {
@@ -42,7 +45,7 @@ function App() {
           />
           <h4>Time reminaing: {timeLeft}</h4>
           <button
-                onClick={() => setStartGame(true)}>
+                onClick={() => setGameStarted(true)}>
                 Start
             </button>
           <h1>Word count: ???</h1>
