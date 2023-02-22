7ad4be44276a1da3215dc3979b39e1ec96416bec
Peter K Crowther <petercrowther@hotmail.com>
2023-02-22 15:47:47 +0000

    PR6 - UPDATE - Context fetch photo data

--- Project_6/capstone/src/context/capstoneContext.js
+++ Project_6/capstone/src/context/capstoneContext.js
@@ -1,9 +1,30 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 const CapstoneContext = React.createContext();
 
 function CapstoneContextProvider(props) {
 
-    const [photos, usePhotos] = useState([]);
+    const [photos, setPhotos] = useState([]);
+    const jsonUrl = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+
+    useEffect(
+        () => {
+            fetch(jsonUrl)
+            .then(response => response.json())
+            .then(data => {
+                setPhotos(data)
+            })        
+        }, 
+        []
+    )
+
+    useEffect(
+        () => {
+            console.log(photos);       
+        }, 
+        [photos]
+    )
+
+
 
     return (
         <CapstoneContext.Provider value={ {photos: photos} }>
