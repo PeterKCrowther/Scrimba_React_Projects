a58f6cbf3e123578607aa9bc58c38bb103b116ea
Peter K Crowther <petercrowther@hotmail.com>
2023-02-25 06:51:25 +0000

    PR6 - ADDITION - Heart and add to cart icon on hover over image

--- Project_6/capstone/src/components/Image.js
+++ Project_6/capstone/src/components/Image.js
@@ -3,25 +3,30 @@ import React, {useState, useEffect} from "react"
 function Image({img, className}) {
 
     const [hover, setHover] = useState(false)
+    const heartIcon = <i className="ri-heart-line favorite"></i>
+    const addToCartIcon = <i className="ri-add-circle-line cart"></i>
 
-    function pointerOverImage() {
-        //console.log("mouse in " + img.id + " - " + hover)
+    function pointerEnteredImgCont() {
         setHover(() => true)
     }
 
-    function pointerLeftImage(){
-        //console.log("mouse out " + img.id + " - " + hover)
+    function pointerExitedImgCont(){
         setHover(() => false)        
     }
 
     return (
-        <div className={`${className} image-container`}>
+        <div 
+            className={`${className} image-container`} 
+            onMouseOver={() => pointerEnteredImgCont()}  
+            onMouseOut={() => pointerExitedImgCont()}
+        >
+            {hover && heartIcon}
+            {hover && addToCartIcon}
             <img 
                 src={img.url} 
                 alt={`img-${img.id}`} 
                 className="image-grid" 
-                onMouseOver={() => pointerOverImage()} 
-                onMouseOut={() => pointerLeftImage()} />
+            />
         </div>
     )
 }
