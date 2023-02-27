f69f3adda9f5e2df22254656160462d1c88baa78
Peter K Crowther <petercrowther@hotmail.com>
2023-02-27 05:52:50 +0000

    PR6 - ADDITION - total cost including locale in cart component

--- Project_6/capstone/src/components/CartItem.js
+++ Project_6/capstone/src/components/CartItem.js
@@ -1,7 +1,7 @@
 import React, {useContext} from "react"
 import {CapstoneContext} from "../context/CapstoneContext"
 
-function CartItem({item}) {
+function CartItem({item, itemCostLocale}) {
 
     const {removeImageFromCart} = useContext(CapstoneContext)
 
@@ -9,7 +9,7 @@ function CartItem({item}) {
         <div className="cart-item">
             <i className="ri-delete-bin-line" onClick={() => removeImageFromCart(item.id)}></i>
             <img src={item.url} alt={item.id} width="130px" />
-            <p>$5.99</p>
+            <p>{itemCostLocale}</p>
         </div>
     )
 }
--- Project_6/capstone/src/pages/Cart.js
+++ Project_6/capstone/src/pages/Cart.js
@@ -1,19 +1,32 @@
-import React, {useContext} from "react"
+import React, {useContext, useState} from "react"
 import {CapstoneContext} from "../context/CapstoneContext.js"
 import CartItem from "../components/CartItem"
 
 function Cart() {
 
+    const langageLocale = "en-GB"
+    const currencyLocale = "GBP"
+
+    const itemCost = 5.99
+    const itemCostLocale = itemCost.toLocaleString(langageLocale, {style: "currency", currency: currencyLocale})
+
     const {cartItems} = useContext(CapstoneContext)
     const cartItemElements = cartItems.map(item => (
-        <CartItem key={item.id} item={item} />
+        <CartItem 
+            key={item.id} 
+            item={item} 
+            itemCostLocale={itemCostLocale} 
+        />
     ))
+
+    const total = itemCost * cartItems.length;
+    const totalLocale = total.toLocaleString(langageLocale, {style: "currency", currency: currencyLocale})
 
     return (
         <main className="cart-page">
             <h1>Check out</h1>
             {cartItemElements}
-            <p className="total-cost">Total: </p>
+            <p className="total-cost">Total: {totalLocale}</p>
             <div className="order-button">
                 <button>Place Order</button>
             </div>            
