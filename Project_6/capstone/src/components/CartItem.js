72a5fad78d04847202fe98f7fc1c9a9bb66ad261
Peter K Crowther <petercrowther@hotmail.com>
2023-02-27 05:16:54 +0000

    PR6 - ADDITION - delete icon removes item from cart onClick

--- Project_6/capstone/src/components/CartItem.js
+++ Project_6/capstone/src/components/CartItem.js
@@ -1,9 +1,13 @@
-import React from "react"
+import React, {useContext} from "react"
+import {CapstoneContext} from "../context/CapstoneContext"
 
 function CartItem({item}) {
+
+    const {removeImageFromCart} = useContext(CapstoneContext)
+
     return (
         <div className="cart-item">
-            <i className="ri-delete-bin-line"></i>
+            <i className="ri-delete-bin-line" onClick={() => removeImageFromCart(item.id)}></i>
             <img src={item.url} alt={item.id} width="130px" />
             <p>$5.99</p>
         </div>
